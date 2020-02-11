ad0a351e6069b9b74fda8dc6b94bf4ee1436160f
Fabian Martinez Portantier <fportantier@securetia.com>
2020-02-11 17:39:25 -0300

    x

--- cmd_block_last.py
+++ cmd_block_last.py
@@ -7,15 +7,14 @@ import requests
 logging.basicConfig()
 
 @click.command()
-@click.argument('index')
 @click.option('-p', 'port', default=5000, help='Port of the local node to manage')
-def cmd_block_get(index, port):
+def cmd_block_last(port):
 
-    r = requests.get(f'http://localhost:{port}/block/{index}')
+    r = requests.get(f'http://localhost:{port}/block/last')
 
     print(json.dumps(r.json(), sort_keys=True))
 
 
 if __name__ == '__main__':
-    cmd_block_get()
+    cmd_block_last()
 
